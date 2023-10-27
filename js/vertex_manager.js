f6c67e3b9651ccaa3da6977270ee7b2b85d00f31
Joel Dietz <jdietz+github@gmail.com>
2023-10-27 11:04:21 +0200

    stub out some more general logic for display

---
+++ js/vertex_manager.js
@@ -0,0 +1,26 @@
+
+
+
+// all render logic
+
+function createScene()  {
+
+}
+
+
+// all fibspiral distribution logic
+
+function createVertices()  {
+
+
+
+}
+
+
+// all resettting / trash collection logic
+
+function clearVertices()  {
+
+
+
+}
